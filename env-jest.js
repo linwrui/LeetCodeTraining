d4d8defd402498cdb9a29741b31e623b1252436c
linwrui <linwrui@163.com>
2020-06-08 22:43:54 +0800

    补充eslint规则

--- env-jest.js
+++ env-jest.js
@@ -1,7 +1,7 @@
-const {ListNode} = require("common-algorithms");
+const { ListNode } = require("common-algorithms");
 // #region [一些扩展方法]
 Array.prototype.toListNode = function (handler) {
-    return ListNode.buildFromArray(this,handler);
+    return ListNode.buildFromArray(this, handler);
 };
 Object.prototype.noInstance = function () {
     return JSON.parse(JSON.stringify(this));
